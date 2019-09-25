da71840bb0bbf950a8469153f331547b2b9ba4cd
Irvin Velarde <irvinvelarde@gmail.com>
2019-09-25 13:07:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ $(document).ready(function () {
 </head>
 <body>
 <nav class="navbar navbar-default">
+        <div class="container">
   <div class="container"> 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
